94340aca25c95d0e2495516694be73a29f36bd60
PriyaA-7 <136041045+PriyaA-7@users.noreply.github.com>
2026-01-22 11:30:24 +0000

    Add SPFx training video player solution

---
+++ training-video-player/src/webparts/trainingVideoPlayer/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface ITrainingVideoPlayerWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'TrainingVideoPlayerWebPartStrings' {
+  const strings: ITrainingVideoPlayerWebPartStrings;
+  export = strings;
+}
